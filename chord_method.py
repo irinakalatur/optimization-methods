4dff19c4506df12a20e3bb80d2e559a5da89aea2
Irina Kalatur <ikalatur9@yandex.ru>
2023-09-27 17:40:00 +0300

    v_1

--- chord_method.py
+++ chord_method.py
@@ -2,40 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-'''Метод бисекции для поиска корня заданной функции
+'''Метод хорд для поиска корня заданной функции
    f: заданная функция
    [a, b]: заданный отрезок
    eps: заданная точность
 '''
-def bisection(f, a, b, eps):
+def chord(f, a, b, eps):
     if f(a) * f(b) > 0:
-        return "Решения нет, либо оно не единственно"      
-    while (b - a) / 2 > eps:
-        c = (a + b) / 2 #находим середину отрезка
-        if f(c) == 0: #если выполняется условие, решение найдено
-            return c
+        return "Решения нет, либо оно не единственно"
+    c = a - f(a)*(b - a) / (f(b) - f(a))      
+    while abs(f(c)) > eps:
         #если f(a)*f(c)<0 то корень расположен на отрезке [a, с]
-        elif f(a) * f(c) < 0:
+        if f(a) * f(c) < 0:
             b = c
         #иначе, на отрезке [с, b]
         else:
             a = c
+        c = a - f(a)*(b - a) / (f(b) - f(a))
     return c
      
 
 def function(x):
     #задаем функцию
-    return x**3 - 10
+    return x**3 - x + 1
 
-x = bisection(function, 1, 4, 0.00000001)
+x = chord(function, -2, -1, 0.001)
 print(f"Корень уравнения x = {x}")
 #построение графика функции
 fig, ax = plt.subplots()
 ax.set_title('График функции')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
-x = np.linspace(1, 4, 70)
-y = x**3 - 10
+x = np.linspace(-2, -1, 70)
+y = x**3 - x + 1
 ax.grid()
 ax.plot(x, y, marker ='o', markersize = 3, markerfacecolor ='red')
 plt.show()
